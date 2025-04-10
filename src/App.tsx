58fd8545b35386d8014b6e4bd6d4c3ee9adcda95
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2025-04-10 17:57:18 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import { Button } from '@nextui-org/react'
+
+function App() {
+  return (
+    <div className="h-screen flex gap-3 items-center justify-center">
+      <h1 className="text-4xl font-bold">🎵 Music Player App</h1>
+      <Button color="danger">Button</Button>
+    </div>
+  )
+}
+
+export default App
