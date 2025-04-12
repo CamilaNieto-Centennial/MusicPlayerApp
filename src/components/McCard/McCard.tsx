eef2c4f3fbaad2ac726c4ed5177f139cb7a718e8
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2025-04-13 00:03:52 +0100

    fix, feat: Fixed McCard & Added Carousel to Popular Albums Array

--- src/components/McCard/McCard.tsx
+++ src/components/McCard/McCard.tsx
@@ -1,22 +1,34 @@
-// src/components/AlbumCard.tsx
-import { Card, CardBody, CardFooter, Image } from '@nextui-org/react'
 import { SpotifyAlbumType } from '../../types/spotify'
+import { MdPlayArrow } from 'react-icons/md'
 
-export const McCard = ({ album }: { album: SpotifyAlbumType }) => (
-  <Card key={album.id} isPressable shadow="sm">
-    <CardBody className="overflow-visible p-0">
-      <Image
+const truncateText = (text: string, maxLength: number) => {
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + '...';
+  }
+  return text;
+};
+
+export const McCard = ({ album, className }: { album: SpotifyAlbumType, className?: string }) => (
+  <div key={album.id} className={`w-fit flex flex-col gap-3 shrink-0 ${className}`}>
+    <div className="relative overflow-hidden rounded-md shadow-sm group">
+      <img
         alt={album.name}
-        className="w-full object-cover h-[140px]"
-        radius="lg"
-        shadow="sm"
+        className="w-[240px] h-[140px] object-cover cursor-pointer transition-all duration-500 ease-in-out group-hover:scale-110"
         src={album.images[0].url}
-        width="100%"
       />
-    </CardBody>
-    <CardFooter className="text-small justify-between">
-      <b>{album.name}</b>
-      <p className="text-default-500">{album.artists[0].name}</p>
-    </CardFooter>
-  </Card>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/40 opacity-0 cursor-pointer group-hover:opacity-100 transition-opacity duration-300 ease-in-out" />
+
+      {/* Play Icon */}
+      <div className="absolute inset-0 flex items-center justify-center opacity-0 cursor-pointer group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+        <div className="bg-white p-3 rounded-full shadow-md transform scale-90 group-hover:scale-100 transition-transform duration-300 ease-in-out">
+          <MdPlayArrow className="text-black w-7 h-7" />
+        </div>
+      </div>
+    </div>
+    <div className="flex flex-col max-w-[240px]">
+      <p className='text-base text-zinc-100 font-medium truncate cursor-pointer hover:text-primary500 transition-all duration-150'>{truncateText(album.name, 26)}</p>
+      <p className='text-sm text-zinc-400 truncate'>{truncateText(album.artists.map((artist) => artist.name).join(', '), 33)}</p>
+    </div>
+  </div>
 )
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useState } from 'react'
-import { Button } from '@nextui-org/react'
+import { Button, Skeleton } from '@nextui-org/react'
 import { getAccessToken, fetchNewAlbumsReleases } from '../api/spotify'
 import { SpotifyAlbumType } from '../types/spotify'
 import { McCard } from '../components'
-
+import { MdPlayArrow } from 'react-icons/md'
 
 export default function Home() {
   const [albums, setAlbums] = useState<SpotifyAlbumType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentSlide, setCurrentSlide] = useState(0); // Track the current slide
+  const itemsPerSlide = 7; // Number of items per slide
+
+  const maxSlide = albums.length - itemsPerSlide
 
   useEffect(() => {
     const loadAlbums = async () => {
-      const token = await getAccessToken()
-      const newReleases = await fetchNewAlbumsReleases(token)
-      setAlbums(newReleases)
-    }
+      try {
+        const token = await getAccessToken();
+        const newReleases = await fetchNewAlbumsReleases(token);
+        setAlbums(newReleases);
+      } catch (error) {
+        console.error('Error fetching albums:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
     loadAlbums()
   }, []);
+
+  // Next and Previous buttons to control the slider
+  const handlePrev = () => {
+    setCurrentSlide(prev => (prev === 0 ? 0 : prev - 1)); // Go back by one item
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((prev) => (prev >= maxSlide ? prev : prev + 1))
+  }
 
   return (
     <main className="flex-1 p-8 overflow-y-auto">
@@ -25,17 +45,79 @@ export default function Home() {
           <h3 className="text-xl font-bold">Popular Albums</h3>
           <p className='font-bold hover:underline cursor-pointer'>Show all</p>
         </div>
-        <div className="gap-2 grid grid-cols-8">
-          {albums.map((album) => (
-            <McCard key={album.id} album={album} />
-          ))}
-        </div>
-      </div>
+        <div className="relative w-full">
+          <div className="overflow-hidden">
+            <div
+              className="flex gap-4"
+            >
+              {isLoading ?
+                <>
+                  {Array.from({ length: 7 }).map((_, i) => (
+                    <div
+                      key={i}
+                      className="flex flex-col gap-3 w-[240px] shrink-0"
+                    >
+                      <Skeleton className="rounded-md">
+                        <div className="w-[240px] h-[140px] rounded-md bg-default-300" />
+                      </Skeleton>
+                      <Skeleton className="w-4/5 rounded-lg">
+                        <div className="h-3 w-4/5 rounded-lg bg-default-200" />
+                      </Skeleton>
+                      <Skeleton className="w-2/5 rounded-lg">
+                        <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+                      </Skeleton>
+                    </div>
+                  ))}
+                </>
+                :
+                <>
+                  {
+                    albums.slice(currentSlide, currentSlide + itemsPerSlide).map((album) => (
+                      <div
+                        key={album.id}
+                        className="flex justify-center"
+                      >
+                        <McCard key={album.id} album={album} className='' />
+                      </div>
+                    ))
+                  }
+                </>
+              }
+            </div>
+          </div>
 
-      <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#07c8f9]'>Outlier</span> Music <span className='text-[#09a6f3]'>Outlier</span> 🎧 <span className='text-[#0a85ed]'>Outlier</span>  🎧 <span className='text-[#0c63e7]'>Outlier</span>  🎧 <span className='text-[#0d41e1]'>Outlier</span></h1>
-      <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#006ba6]'>Outlier</span> Music <span className='text-[#0496ff]'>Outlier</span> 🎧 <span className='text-[#ffbc42]'>Outlier</span>  🎧 <span className='text-[#d81159]'>Outlier</span>  🎧 <span className='text-[#8f2d56]'>Outlier</span></h1>
-      <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#42BBFF]'>Outlier</span> Music <span className='text-[#0496ff]'>Outlier</span> 🎧 <span className='text-[#0274DB]'>Outlier</span>  🎧 <span className='text-[#0256B7]'>Outlier</span>  🎧 <span className='text-[#013D93]'>Outlier</span></h1>
-      <Button color="danger">Button</Button>
-    </main>
+          {/* Previous Button */}
+          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 px-3 z-10">
+            <Button
+              size="sm"
+              variant="flat"
+              isIconOnly
+              onClick={handlePrev}
+              className="bg-black/40 text-white border-none hover:bg-black/60 transition-all duration-300"
+            >
+              <MdPlayArrow />
+            </Button>
+          </div>
+
+          {/* Next Button */}
+          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 px-3 z-10">
+            <Button
+              size="sm"
+              variant="flat"
+              isIconOnly
+              onClick={handleNext}
+              className="bg-black/40 text-white border-none hover:bg-black/60 transition-all duration-300"
+            ><MdPlayArrow />
+            </Button>
+          </div>
+
+        </div>
+
+        <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#07c8f9]'>Outlier</span> Music <span className='text-[#09a6f3]'>Outlier</span> 🎧 <span className='text-[#0a85ed]'>Outlier</span>  🎧 <span className='text-[#0c63e7]'>Outlier</span>  🎧 <span className='text-[#0d41e1]'>Outlier</span></h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#006ba6]'>Outlier</span> Music <span className='text-[#0496ff]'>Outlier</span> 🎧 <span className='text-[#ffbc42]'>Outlier</span>  🎧 <span className='text-[#d81159]'>Outlier</span>  🎧 <span className='text-[#8f2d56]'>Outlier</span></h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome to <span className='text-[#42BBFF]'>Outlier</span> Music <span className='text-[#0496ff]'>Outlier</span> 🎧 <span className='text-[#0274DB]'>Outlier</span>  🎧 <span className='text-[#0256B7]'>Outlier</span>  🎧 <span className='text-[#013D93]'>Outlier</span></h1>
+        <Button color="danger">Button</Button>
+      </div>
+    </main >
   )
 }
